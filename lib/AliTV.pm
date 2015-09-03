78b4b96a4ff913963fdfb251a2b71ad55cf6bbc6
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-09-03 19:38:48 +0200

    Initialized an empty perl module skeleton

---
+++ lib/AliTV.pm
@@ -0,0 +1,60 @@
+package AliTV;
+
+use 5.010000;
+use strict;
+use warnings;
+
+our @ISA = qw();
+
+our $VERSION = '0.1';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+AliTV - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use AliTV;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for AliTV, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Frank Foerster, E<lt>frf53jh@E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 by Frank Foerster
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.18.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
