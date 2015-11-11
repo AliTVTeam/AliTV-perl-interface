02dcb57ac5897964ec102b0f2d4ea5cd747b1e50
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-11-11 09:38:54 +0100

    Added a basic AliTV::Feature class

---
+++ lib/AliTV/Feature.pm
@@ -0,0 +1,35 @@
+package AliTV::Feature;
+
+1;
+
+=head1 AliTV::Feature
+
+AliTV::Feature - The AliTV class representing a features for a sequence object
+
+=head1 SYNOPSIS
+
+  use AliTV::Feature;
+
+  my $seq_obj = AliTV::Feature->new(-file => 'input.gff');
+  
+=head1 DESCRIPTION
+
+This is the AliTV class to represent features for sequence objects. The feature information can be 
+loaded from a file which format is supported by Bioperl. 
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Frank Foerster, E<lt>foersterfrank@gmx.deE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 by Frank Foerster
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
