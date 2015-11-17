32d5d6746f389217576e94a579857b81db196593
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-11-17 18:34:06 +0100

    Overwritten AliTV::Base::_initialize()

--- lib/AliTV/Tree.pm
+++ lib/AliTV/Tree.pm
@@ -6,4 +6,15 @@ use warnings;
 
 use parent qw(AliTV::Base);
 
+use Bio::Tree::TreeI;
+use Bio::Tree::Node;
+
+sub _initialize
+{
+    my $self = shift;
+
+    # create storage for the tree object
+    $self->{_tree} = undef;
+}
+
 1;
