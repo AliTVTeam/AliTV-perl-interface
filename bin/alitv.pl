5ce69a97bdf4cd3d7ff7eb9a23a6418f39dc4788
Frank Förster <foersterfrank@gmx.de>
2016-01-20 10:37:00 +0100

    Added a perl script alitv.pl
    The main script was added and should be installed in the final location.
    
    No checks are performed for that script. But currently, it has no function at
    all. Later all functionality should be included into the AliTV class anyway.

---
+++ bin/alitv.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+alitv.pl - generate the required JSON files for AliTV
+
+=head1 SYNOPSIS
+
+alitv.pl options.yml
+
+=head1 DESCRIPTION
+
+The script creates the required JSON files to run AliTV. The two
+output files are required to load into the AliTV website to use AliTV
+for the visualization of multiple whole genome alignments.
+
+=head1 AUTHOR
+
+Frank FE<246>rster E<lt>foersterfrank@gmx.deE<gt>
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+L<AliTV-Demo-Page|http://bioinf-wuerzburg.github.io/AliTV/d3/AliTV.html>
+
+=item *
+L<AliTV-Website|http://bioinf-wuerzburg.github.io/AliTV/>
+
+=item *
+L<AliTV-Github-Page|https://github.com/BioInf-Wuerzburg/AliTV>
+
+
+=back
+
+=cut
