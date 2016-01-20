19538279e56b85118bbb6a01a419de934a918d36
Frank Förster <foersterfrank@gmx.de>
2016-01-20 11:10:40 +0100

    The class AliTV was prepared

--- lib/AliTV.pm
+++ lib/AliTV.pm
@@ -4,57 +4,35 @@ use 5.010000;
 use strict;
 use warnings;
 
-our @ISA = qw();
+use parent 'AliTV::Base';
 
 our $VERSION = '0.1';
 
 
-# Preloaded methods go here.
-
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
+=pod
 
 =head1 NAME
 
-AliTV - Perl extension for blah blah blah
+AliTV - Perl class for the alitv script which generates the JSON input for AliTV
 
 =head1 SYNOPSIS
 
   use AliTV;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for AliTV, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
+The class AliTV implements the functionality for the alitv.pl script.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
 =head1 AUTHOR
 
-Frank Foerster, E<lt>frf53jh@E<gt>
+Frank FE<246>ster E<lt>foersterfrank@gmx.deE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2015 by Frank Foerster
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.18.2 or,
-at your option, any later version of Perl 5 you may have available.
-
+See the F<LICENCE> file for information about the licence.
 
 =cut
