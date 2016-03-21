294b5d64a159507c46dc782140ea5098dd2a2f92
Frank Förster <foersterfrank@gmx.de>
2016-03-21 21:23:33 +0100

    Implemented AliTV::_make_and_set_uniq_seq_names()

--- lib/AliTV.pm
+++ lib/AliTV.pm
@@ -47,6 +47,16 @@ sub run
 
     $self->_import_genomes();
 
+    #################################################################
+    #
+    # Create uniq sequence names
+    #
+    #################################################################
+    # if the names are already uniq, the sequences names will be used
+    # as unique names, otherwise the sequences will be numbered to
+    # generate unique names
+
+    $self->_make_and_set_uniq_seq_names();
 }
 
 sub file
@@ -109,6 +119,52 @@ sub _get_default_settings
     my $default = YAML::Load($self->{_default_yml});
 
     return $default;
+}
+
+sub _make_and_set_uniq_seq_names
+{
+    my $self = shift;
+
+    # get a list of all sequence names
+
+    my @all_seq_ids = ();
+
+    foreach my $genome_id (sort keys %{$self->{_genomes}})
+    {
+	push(@all_seq_ids, map { {name => $_, genome => $genome_id} } (sort $self->{_genomes}{$genome_id}->get_seq_names()));
+    }
+
+    # check if the sequence names are uniq
+    my %seen = ();
+
+    foreach my $curr (@all_seq_ids)
+    {
+	$seen{$curr->{name}}++;
+    }
+
+    # if the number of keys is equal to the number of total sequences,
+    # they should be uniq
+    if ((keys %seen) == @all_seq_ids)
+    {
+	# sequence names are uniq and can be used as uniq names
+	@all_seq_ids = map { {name => $_->{name}, genome => $_->{genome}, uniq_name => $_->{name}} } (@all_seq_ids);
+    } else {
+	# sequences names are not uniq! Therefore, generate new
+	# sequence names
+
+	my $counter = 0;
+
+	@all_seq_ids = map { {name => $_->{name}, genome => $_->{genome}, uniq_name => "seq".$counter++ } } (@all_seq_ids);
+    }
+
+    # set the new uniq names for each genome
+    foreach my $genome_id (keys %{$self->{_genomes}})
+    {
+	my @set_list = map { $_->{uniq_name} => $_->{name} } grep {$_->{genome} eq $genome_id } @all_seq_ids;
+
+	$self->{_genomes}{$genome_id}->set_uniq_seq_names(@set_list);
+    }
+
 }
 
 1;
