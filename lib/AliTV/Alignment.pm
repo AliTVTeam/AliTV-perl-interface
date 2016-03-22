9ecd4bfee73be00a8ec2111742dc066109ed13d6
Frank Förster <foersterfrank@gmx.de>
2016-03-22 13:10:10 +0100

    Added a AliTV::Alignment class

---
+++ lib/AliTV/Alignment.pm
@@ -0,0 +1,35 @@
+package AliTV::Alignment;
+
+1;
+
+=head1 AliTV::Alignment
+
+AliTV::Alignment - The AliTV::Alignment class for interaction
+with different alignment programs
+
+=head1 SYNOPSIS
+
+  use AliTV::Alignment::type;
+
+  # call mechanism not clear so far
+  
+=head1 DESCRIPTION
+
+This is the AliTV::Alignment class to align a given sequence set and return the generated alignments.
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Frank Foerster, E<lt>foersterfrank@gmx.deE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 by Frank Foerster
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
