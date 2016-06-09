0a596e98e72ccbcfd321d1fe6d0856fe6d2e8c6c
Frank Förster <foersterfrank@gmx.de>
2016-06-09 11:13:49 +0200

    Lastz will be called now and all alignmentfiles are returned

--- lib/AliTV/Alignment/lastz.pm
+++ lib/AliTV/Alignment/lastz.pm
@@ -6,6 +6,8 @@ use warnings;
 use parent 'AliTV::Alignment';
 
 use File::Which;
+use IPC::System::Simple qw(capturex systemx);
+use File::Temp qw(:seekable);
 
 sub _check
 {
@@ -24,6 +26,61 @@ sub _check
 sub run
 {
     my $self = shift;
+
+    my @seq_set = @_;
+
+    $self->_check();
+
+    # first, we need to create the database file
+
+    # create a temporary file
+    my $db = File::Temp->new(
+	TEMPLATE => 'tempXXXXX',
+	SUFFIX => '.fasta'
+	);
+
+    # create the database file
+    my $db_obj = Bio::SeqIO->new(-fh => $db, -format => "fasta");
+    foreach my $seq (@seq_set)
+    {
+	my $seq_renamed = $seq->clone();
+
+	$seq_renamed->id("db_".$seq->id());
+
+	$db_obj->write_seq($seq_renamed);
+    }
+
+    my $db_file=$db->filename().'[multiple]';
+
+    my @alignments = ();
+
+    $self->_debug("Starting alignment generation...");
+
+    foreach my $seq (@seq_set)
+    {
+	my $query = File::Temp->new(
+	    TEMPLATE => 'tempXXXXX',
+	    SUFFIX => '.fasta'
+	    );
+
+	my $query_obj = Bio::SeqIO->new(-fh => $query, -format => "fasta");
+
+	$query_obj->write_seq($seq);
+
+	my $aln_file = File::Temp->new(
+	    TEMPLATE => 'tempXXXXX',
+	    SUFFIX => '.maf'
+	    );
+
+	my $cmd = join(" ", $self->{_cmd}, ($db_file, $query->filename(), "--output=".$aln_file->filename(), split(/ /, $self->{_parameters})));
+	$self->_debug($cmd);
+	systemx($self->{_cmd}, ($db_file, $query->filename(), "--output=".$aln_file->filename(), split(/ /, $self->{_parameters})));
+
+	push(@alignments, $aln_file);
+    }
+
+    $self->_debug("Finished alignment generation");
+
 }
 
 1;
@@ -38,7 +95,7 @@ with an alignment program
   use AliTV::Alignment::lastz;
 
   # call mechanism not clear so far
-  
+
 =head1 DESCRIPTION
 
 This is the AliTV::Alignment class to align a given sequence set and return the generated alignments.
