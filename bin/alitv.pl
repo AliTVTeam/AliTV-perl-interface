9a31f7f039339b26e1139a5636660c9339551028
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-08-08 16:49:24 +0200

    Added a project and an output parameter

--- bin/alitv.pl
+++ bin/alitv.pl
@@ -17,17 +17,23 @@ use YAML;
 my $man = 0;
 my $help = 0;
 
-my ($logfile);
+my ($project, $logfile, $output);
 
 GetOptions(
     'help|?' => \$help,
     man => \$man,
+    'project=s' => \$project,
     'logfile=s' => \$logfile,
+    'output=s' => \$output,
     ) or pod2usage(2);
 
 pod2usage(1) if ($help || @ARGV== 0);
 pod2usage(-exitval => 0, -verbose => 2) if $man;
 
+# generate a uniq project name if not specified and a log file name
+# accordingly if also not specified
+
+($project, $output, $logfile) = generate_filenames($project, $output, $logfile);
 
 # Log4Perl configuration
 my $conf = q(
@@ -72,20 +78,50 @@ if (@ARGV == 1)
 	push(@{$config->{genomes}}, {name => $infile, sequence_files => [ $infile ]});
     }
 
-    my $fh;
-    ($fh, $yml) = File::Temp::tempfile("autogen_XXXXXXX", SUFFIX => ".yml" );
-    close($fh) || die "Unable to close file '$yml': @!\n";
-
-    YAML::DumpFile($yml, $config);
+    YAML::DumpFile($project.".yml", $config);
 
     $logger->info("Wrote temporary YAML file '$yml'");
 }
 
 my $obj = AliTV->new(-file => $yml);
 
-my $output = $obj->run();
+my $outputfh;
 
-print $output;
+if ($output eq "-" || $output eq "")
+{
+    $outputfh = *STDOUT;
+} else {
+    open($outputfh, ">", $output) || die "Unable to open file '$output' for writing: $!";
+}
+
+print $outputfh $obj->run();
+
+close($outputfh) || die "Unable to close file '$output' after writing: $!";
+
+sub generate_filenames
+{
+    my ($project, $output, $logfile) = @_;
+
+    unless (defined $project)
+    {
+	my ($fh, $fn) = File::Temp::tempfile("autogen_XXXXXXX");
+	close($fh) || die "Unable to close file '$yml': @!\n";
+
+	$project = $fn;
+    }
+
+    unless (defined $output)
+    {
+	$output = $project.".json";
+    }
+
+    unless (defined $logfile)
+    {
+	$logfile = $project.".log";
+    }
+
+    return ($project, $output, $logfile);
+}
 
 sub logfile()
 {
@@ -125,9 +161,22 @@ visualization of multiple whole genome alignments.
 
 =over 4
 
+=item --project  Project name
+
+The name of the project will be the given argument. If this parameter
+was not provided, one project name will be auto generated. This will
+be the base name for the log file, the yml file, and the output file.
+
+=item --output   Output file
+
+The name of the output file. If non is provided, the output file name
+will be based on the project name. If STDOUT should be used, please
+set the output filename to C<-> via option C<alitv.pl --output ->.
+
 =item --logfile   Log file
 
-The name of the log file.
+The name of the log file. If non is provided, the log file name will
+be based on the project name.
 
 =back
 
